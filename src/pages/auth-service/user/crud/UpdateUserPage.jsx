3eb5a743162eef1f20d2792c6abf4269f59eacdd
Daniel-615 <suyanangel615@gmail.com>
2025-07-31 16:28:21 -0600

    Feat: added profile to update

--- src/pages/auth-service/user/crud/UpdateUserPage.jsx
+++ src/pages/auth-service/user/crud/UpdateUserPage.jsx
@@ -1,8 +1,8 @@
 import { useForm } from 'react-hook-form';
 import { useState, useEffect } from 'react';
-import { updateUser } from '../../../../api-gateway/user.crud.js';
+import { updateUser,deactivateAccount } from '../../../../api-gateway/user.crud.js';
 import { useNavigate } from 'react-router-dom';
-
+import { Logout } from '../../../../api-gateway/auth.js';
 function UpdateUserPage() {
   const { register, handleSubmit, setValue } = useForm();
   const [serverMessage, setServerMessage] = useState(null);
@@ -35,6 +35,9 @@ function UpdateUserPage() {
         ...data
       };
       localStorage.setItem("user", JSON.stringify(updatedUser));
+      setValue('nombre', data.nombre);
+      setValue('apellido',data.apellido);
+      setValue('email',data.email);
       setTimeout(() => navigate('/'), 1500);
     } else {
       setServerMessage(response.error);
@@ -79,6 +82,25 @@ function UpdateUserPage() {
           <p className="text-green-400 text-sm mt-4 text-center">{successMessage}</p>
         )}
       </form>
+      <button
+        className="w-full mt-4 bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-700 transition-colors"
+        onClick={async () => {
+          const confirm = window.confirm("¿Estás seguro de que deseas desactivar tu cuenta?");
+          if (!confirm) return;
+
+          const result = await deactivateAccount(userId);
+
+          if (result.success) {
+            alert("Cuenta desactivada exitosamente.");
+            await Logout();
+          } else {
+            setServerMessage(result.error || "No se pudo desactivar la cuenta.");
+          }
+        }}
+      >
+        Desactivar cuenta
+      </button>
+
     </div>
   );
 }
